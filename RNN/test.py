fb23dfe1fe398558cd4b17f7a41fa7c3d698be66
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-05-10 15:51:22 +0300

    setup environment

---
+++ RNN/test.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import cv2
+import numpy as np
+import matplotlib as mpl
+import mediapipe as mp
+
+print(tf.__version__)
+print(cv2.__version__)
+print(np.__version__)
+print(mpl.__version__)
+print(mp.__version__)
