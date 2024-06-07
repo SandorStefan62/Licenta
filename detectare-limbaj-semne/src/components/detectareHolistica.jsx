7a13d93b2dec5289d9d1c95983ef11872c15d159
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-06-07 05:09:21 +0300

    stilizare puncte cheie

--- detectare-limbaj-semne/src/components/detectareHolistica.jsx
+++ detectare-limbaj-semne/src/components/detectareHolistica.jsx
@@ -1,6 +1,18 @@
 import React, { useRef, useEffect } from "react"
-import { Holistic } from "@mediapipe/holistic"
+import {
+    Holistic,
+    POSE_LANDMARKS,
+    HAND_CONNECTIONS,
+    FACEMESH_TESSELATION,
+    FACEMESH_RIGHT_EYE,
+    FACEMESH_RIGHT_EYEBROW,
+    FACEMESH_LEFT_EYE,
+    FACEMESH_LEFT_EYEBROW,
+    FACEMESH_FACE_OVAL,
+    FACEMESH_LIPS
+} from "@mediapipe/holistic"
 import * as Camera from "@mediapipe/camera_utils"
+import * as drawingUtils from "@mediapipe/drawing_utils"
 
 
 function DetectareHolistica() {
@@ -18,24 +30,32 @@ function DetectareHolistica() {
         canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
 
         //deseneaza punctele cheie ale fetei
-        if (results.faceLandmarks) {
-            for (const landmark of results.faceLandmarks) {
-                canvasCtx.beginPath();
-                canvasCtx.arc(landmark.x * canvasElement.width, landmark.y * canvasElement.height, 1, 0, 2 * Math.PI);
-                canvasCtx.fillStyle = 'rgba(255, 255, 0, 0.5)';
-                canvasCtx.fill();
-            }
-        }
+        // if (results.segmentationMask) {
+        //     canvasCtx.drawImage(results.segmentationMask, 0, 0, canvasElement.width, canvasElement.height);
+
+        //     canvasCtx.globalCompositeOperation = 'source-in';
+        //     canvasCtx.fillStyle = '#00FF007F';
+        //     canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
+
+        //     canvasCtx.globalCompositeOperation = 'destination-atop';
+        //     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+        //     canvasCtx.globalCompositeOperation = 'source-over';
+        // }
+        // else {
+        //     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+        // }
 
         //deseneaza punctele cheie ale corpului
+        canvasCtx.lineWidth = 5;
         if (results.poseLandmarks) {
             for (const landmark of results.poseLandmarks) {
                 canvasCtx.beginPath();
                 canvasCtx.arc(landmark.x * canvasElement.width, landmark.y * canvasElement.height, 2, 0, 2 * Math.PI);
-                canvasCtx.fillStyle = 'rgba(0, 255, 255, 0.5';
+                canvasCtx.fillStyle = "rgba(0, 255, 255, 0.5)";
                 canvasCtx.fill();
             }
         }
+
 
         //deseneaza pucntele cheie ale mainii stangi
         if (results.leftHandLandmarks) {
@@ -57,6 +77,32 @@ function DetectareHolistica() {
             }
         }
 
+        drawingUtils.drawConnectors(canvasCtx, results.poseLandmarks, POSE_LANDMARKS, { color: 'white' });
+
+
+        drawingUtils.drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, { color: 'white' });
+        drawingUtils.drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+            color: 'white',
+            fillColor: 'rgb(0,217,231)',
+            lineWidth: 2,
+            radius: (data) => drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1)
+        });
+        drawingUtils.drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, { color: 'white' });
+        drawingUtils.drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+            color: 'white',
+            fillColor: 'rgb(255,138,0)',
+            lineWidth: 2,
+            radius: (data) => drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1)
+        });
+
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, { color: '#C0C0C070', lineWidth: 1 });
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_RIGHT_EYE, { color: 'rgb(0,217,231)' });
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_RIGHT_EYEBROW, { color: 'rgb(0,217,231)' });
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_LEFT_EYE, { color: 'rgb(255,138,0)' });
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_LEFT_EYEBROW, { color: 'rgb(255,138,0)' });
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_FACE_OVAL, { color: '#E0E0E0', lineWidth: 5 });
+        drawingUtils.drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_LIPS, { color: '#E0E0E0', lineWidth: 5 });
+
         canvasCtx.restore();
     }
 
@@ -68,6 +114,7 @@ function DetectareHolistica() {
 
             holistic.setOptions({
                 modelComplexity: 1,
+                selfieMode: true,
                 smoothLandmarks: true,
                 enableSegmentation: true,
                 smoothSegmentation: true,
@@ -87,8 +134,8 @@ function DetectareHolistica() {
                             console.error("Eroare la trimiterea video-ului spre MediaPipe: ", error);
                         }
                     },
-                    width: 640,
-                    height: 480,
+                    width: 1920,
+                    height: 1080,
                 });
                 camera.start();
             }
@@ -102,7 +149,7 @@ function DetectareHolistica() {
     return (
         <div>
             <video ref={videoRef} style={{ display: 'none' }}></video>
-            <canvas ref={canvasRef} width="640" height="480"></canvas>
+            <canvas ref={canvasRef} width="960" height="540"></canvas>
         </div>
     )
 }
