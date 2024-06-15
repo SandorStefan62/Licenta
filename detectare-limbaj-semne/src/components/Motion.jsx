b70e437e3577fb951e8b58debc69c9806bb6fae7
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-06-15 09:04:44 +0300

    actualizat animatia de tranzitie intre pagini

--- detectare-limbaj-semne/src/components/Motion.jsx
+++ detectare-limbaj-semne/src/components/Motion.jsx
@@ -3,9 +3,16 @@ import { motion } from "framer-motion"
 function Motion(Component) {
     return function HOC() {
         return (
-            <motion.div initial={{ y: 500 }} animate={{ y: 0, transition: { duration: 0.5, type: "spring" } }} exit={{ y: -500, transition: { duration: 0.5, type: "spring", ease: "easeInOut" } }} >
-                <Component />
-            </motion.div>
+            <div className="ml-14 w-full h-full flex justify-center items-center">
+                <motion.div
+                    initial={{ opacity: 0.1, scale: 0.95 }}
+                    animate={{ opacity: 1, scale: [null, 1.02, 1] }}
+                    transition={{ duration: 0.2 }}
+                    className="w-9/10 min-h-9/10 h-[1px] h-auto rounded-[30px] flex flex-col justify-center items-center bg-background-color-lower-alpha"
+                >
+                    <Component />
+                </motion.div>
+            </div>
         )
     }
 }
