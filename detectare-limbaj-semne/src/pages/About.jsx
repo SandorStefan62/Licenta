722250073cb01388b8a94d932ba20f449d83d7cd
SandorStefan62 <sandor.stefanandrei02@gmail.com>
2024-06-19 00:24:03 +0300

    reglari minore

--- detectare-limbaj-semne/src/pages/About.jsx
+++ detectare-limbaj-semne/src/pages/About.jsx
@@ -3,8 +3,8 @@ import Motion from "../components/Motion";
 
 function AboutComponent() {
     return (
-        <div className="About">
-            <h1>About Page!</h1>
+        <div className="w-full h-full absolute flex flex-row justify-center items-center gap-4">
+            <h1 className="text-5xl">Home Page!</h1>
         </div>
     )
 }
--- detectare-limbaj-semne/src/pages/Home.jsx
+++ detectare-limbaj-semne/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import Motion from "../components/Motion";
 
 function HomeComponent() {
     return (
-        <div className="About">
-            <h1>Home Page!</h1>
+        <div className="w-full h-full absolute flex flex-row justify-center items-center gap-4">
+            <h1 className="text-5xl">Home Page!</h1>
         </div>
     )
 }
